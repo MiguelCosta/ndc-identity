4687414998a642db0a1394bf5d6516062d083679
Miguel Pinto Costa <miguelpinto.costa@farfetch.com>
2019-02-26 14:03:21 +0000

    feat: add 01 exercice dev

---
+++ 01_AuthenticationAuthorization/dev/MoviesWebApp/src/MoviesWebApp/Startup.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace MoviesWebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMoviesLibrary();
+
+            // TODO: add authentication service to DI
+            // set the default scheme to "Cookies"
+
+            // TODO: add the cookie handler
+            // set login and access denied paths ("/Account/Login" and "/Account/AccessDenied")
+            services.AddAuthentication("Cookies")
+                .AddCookie("Cookies", options =>
+                {
+                    options.LoginPath = "/Account/Login";
+                    options.AccessDeniedPath = "/Account/AccessDenied";
+                });
+
+            // TODO: add authorization services to DI
+            services.AddAuthorization();
+
+            // TODO: configure a "SearchPolicy" to only allow authenticated customers and admins
+
+            // TODO: add the PolicyServer.Local services to DI
+
+            // TODO: register the authorization handlers in DI
+
+            // Add framework services.
+            services.AddMvc(options =>
+            {
+                // TODO: add a AuthorizeFilter that uses a policy that prevents anonymous access
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            // TODO: add the authentication middleware
+            app.UseAuthentication();
+
+            // TODO: add the claims transformation middleware from PolicyServer.Local
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
